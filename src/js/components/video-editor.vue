f5aa8f1c2a3c542fc48a3a5a3af740b157dfa128
Roman, Greg <greg.roman@teamaol.com>
2017-03-14 17:04:15 -0400

    removed hidden template options.  cleaned up form.

--- src/js/components/video-editor.vue
+++ src/js/components/video-editor.vue
@@ -4,18 +4,26 @@
         <div v-if="slides.length" class="grid grid-small">
             <div class="cell-m-4" v-for="slide in slides" key="slide.id">
                 <thumbnail :image="slide.image_url_thumb"></thumbnail>
-                <select v-model="slide.template">
-                    <option v-for="template in templates" :value="template">
-                        {{ template.title }}
-                    </option>
-                </select>
-                <input v-model="slide.title" placeholder="Add a title" :value="slide.title">
-                <textarea v-model="slide.caption" placeholder="Add a caption">
-                    {{ slide.caption }}
-                </textarea>
-                <label>
-                    <input type="checkbox" v-model="slide.bumper"> Add Bumper
-                </label>
+                <div class="form-control">
+                    <select v-model="slide.template">
+                        <option v-for="template in templates" v-if="template.visible" :value="template">
+                            {{ template.title }}
+                        </option>
+                    </select>
+                </div>
+                <div class="form-control">
+                    <input v-model="slide.title" placeholder="Add a title" :value="slide.title" type="text">
+                </div>
+                <div class="form-control">
+                    <textarea v-model="slide.caption" placeholder="Add a caption">
+                        {{ slide.caption }}
+                    </textarea>
+                </div>
+                <div class="form-control">
+                    <label>
+                        <input type="checkbox" v-model="slide.bumper"> Add Bumper
+                    </label>
+                </div>
             </div>
         </div>
         <loading-indicator v-else></loading-indicator>
