285e0cd53a97c3d597497c3624be0c653fe960ce
Roman, Greg <greg.roman@teamaol.com>
2017-03-29 09:57:55 -0400

    added link to tutorial

--- src/js/components/app-bar.vue
+++ src/js/components/app-bar.vue
@@ -1,13 +1,18 @@
 <style scoped></style>
 <template>
     <div class="app-bar flex-shrink-0">
-        <router-link v-if="config.buttonLeft === 'back'" :to="{ name: 'home' }" exact class="button button-blue button-left">
-            &lsaquo; All Projects
-        </router-link>
         <h1 v-if="config.title" class="app-bar-title text-center">{{ config.title }}</h1>
-        <router-link :to="{ name: 'uploads' }" exact class="button button-blue button-right">
-            Uploads
-        </router-link>
+        <div class="button-group buttons-left">
+            <router-link v-if="config.buttonLeft === 'back'" :to="{ name: 'home' }" exact class="button button-blue">
+                &lsaquo; All Projects
+            </router-link>
+        </div>
+        <div class="button-group buttons-right">
+            <a class="button button-blue" href="https://s3.amazonaws.com/alpha-global-origin/daily-video/Video_Tool_Tutorial.mp4" target="_blank">View Tutorial</a>
+            <router-link :to="{ name: 'uploads' }" exact class="button button-blue">
+                Uploads
+            </router-link>
+        </div>
     </div>
 </template>
 <script>
