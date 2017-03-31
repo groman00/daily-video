3a37a96fd1b57063f236afa3b9652c90421f18a1
Roman, Greg <greg.roman@teamaol.com>
2017-03-31 11:26:45 -0400

    clearing any queued jobs on server start up

--- db.js
+++ db.js
@@ -1,3 +1,10 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Jobs = require('./lib/Models/Job');
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost:27017/daily-video');
+
+// Clear all jobs on startup
+Jobs.remove({}, () => {
+    console.log('cleared existing jobs');
+});
+
