fd87288947b19ea19f6530a71c4623b2fa38fc19
Roman, Greg <greg.roman@teamaol.com>
2017-07-11 12:52:14 -0400

    configured builds for dev and prod.

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,37 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WebpackManifestPlugin = require('webpack-manifest-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const pkg = require('./package.json');
+const env = process.env.NODE_ENV;
 const src = {
-    js: './src/js'
+    js: './src/js',
+    filename: 'main.js'
 };
 const dist = {
     public: './public',
     js: '/js',
-    css: '/css'
+    css: '/css',
+    filename: 'bundle'
 };
-
-module.exports = {
-    entry: `${src.js}/main.js`,
+let plugins = [
+    new CleanWebpackPlugin([
+        `${dist.public}${dist.js}`,
+        `${dist.public}${dist.css}`,
+    ]),
+    new WebpackManifestPlugin(),
+    new webpack.EnvironmentPlugin({
+        NODE_ENV: env
+    }),
+    new webpack.DefinePlugin({
+        __VERSION__: JSON.stringify(pkg.version)
+    })
+];
+const config = {
+    entry: `${src.js}/${src.filename}`,
     output: {
-        path: path.join(__dirname, dist.public),
-        filename: `${dist.js}/[name].[chunkhash].js`,
+        path: path.join(__dirname, dist.public)
     },
-    plugins: [
-        new WebpackManifestPlugin(),
-        new webpack.EnvironmentPlugin({
-            NODE_ENV: 'production'
-            // NODE_ENV: 'development'
-        }),
-        new ExtractTextPlugin({
-            filename: `${dist.css}/[name].[chunkhash].css`,
-            disable: false,
-            allChunks: true
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new OptimizeCssAssetsPlugin({
-            cssProcessorOptions: { discardComments: { removeAll: true } }
-        }),
-        new webpack.DefinePlugin({
-            __VERSION__: JSON.stringify(pkg.version)
-        })
-    ],
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.esm'
@@ -69,3 +61,33 @@ module.exports = {
         ]
     }
 };
+
+if (env === 'production') {
+    // Configure Prod
+    config.output.filename = `${dist.js}/${dist.filename}.[chunkhash].js`;
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorOptions: { discardComments: { removeAll: true } }
+        }),
+        new ExtractTextPlugin({
+            filename: `${dist.css}/${dist.filename}.[chunkhash].css`
+        })
+    );
+} else {
+    // Configure Dev
+    config.output.filename = `${dist.js}/${dist.filename}.js`;
+    plugins.push(
+        new ExtractTextPlugin({
+            filename: `${dist.css}/${dist.filename}.css`
+        })
+    );
+}
+
+config.plugins = plugins;
+
+module.exports = config;
